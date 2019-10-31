f457db4be71e851cc70aecace24b92d6a3f94387
Aaron victor winnercoz <Winnercoz340@gmail.com>
2019-10-30 21:52:22 -0800

    working but cached error found

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ProductService} from '../product.service';
+import { ProductService } from "../ProductService";
 import {Album} from '../album'
 
 
